9f819324f3ba1210df018fd193501a5c6dbcb5e1
cbranum <tr1uv13q>
2014-01-22 22:16:45 -0600

    added support for shake gesture

--- BandNameGenerator/ViewController.m
+++ BandNameGenerator/ViewController.m
@@ -9,36 +9,38 @@
 #import "ViewController.h"
 
 @interface ViewController ()
-@property (nonatomic, strong) UIButton *buttonGenerate;
 @property (nonatomic, strong) UILabel *labelBandName;
 @end
 
 @implementation ViewController
+
+- (NSUInteger)supportedInterfaceOrientations
+{
+    // Support only portrait mode
+    return UIInterfaceOrientationMaskPortrait;
+}
 
 - (void)viewDidLoad
 {
     [super viewDidLoad];
     
     CGRect screenRect = [[UIScreen mainScreen] bounds];
-    CGFloat screenWidth = screenRect.size.width;
     CGFloat screenHeight = screenRect.size.height;
     
 	// Do any additional setup after loading the view, typically from a nib.
     
-    self.buttonGenerate = [UIButton buttonWithType:UIButtonTypeRoundedRect];
-    self.buttonGenerate.frame = CGRectMake(20.0f, 80.0f, 280.0f, 4.0f);
-    [self.buttonGenerate setTitle:@"Generate" forState:UIControlStateNormal];
-    [self.view addSubview:self.buttonGenerate];
-    
-    self.labelBandName = [[UILabel alloc] initWithFrame:CGRectMake(0.0f, 0.0f, 300.0f, 23.0f)];
+    self.labelBandName = [[UILabel alloc] initWithFrame:CGRectMake(40.0f, 0.0f, 300.0f, 250.0f)];
     self.labelBandName.center = self.view.center;
+    self.labelBandName.textColor = [UIColor whiteColor];
+    [self.labelBandName setFont:[UIFont fontWithName:@"HelveticaNeue-Light" size:38]];
+    [self.labelBandName setNumberOfLines:2];
+    self.labelBandName.textAlignment = NSTextAlignmentCenter;
+    [self.labelBandName setText: [self getRandomBandName]];
     [self.view addSubview:self.labelBandName];
     
-    // setup event handler
-    [self.buttonGenerate addTarget:self action:@selector(handleGenerate:) forControlEvents:UIControlEventTouchUpInside];
-    
-    // default background color
-    // self.view.backgroundColor = [UIColor darkGrayColor];
+    // setup event handler for tapping anywhere on screen
+    UITapGestureRecognizer *gr = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(handleGenerate:)];
+    [self.view addGestureRecognizer:gr];
     
     //add background image
     UIImage *background = [UIImage imageNamed: @"background-screen.png"];
@@ -47,47 +49,52 @@
     [self.view addSubview:imageView];
     [self.view sendSubviewToBack:imageView];
     
-    //NSLog(@"%f", self.view.bounds.size.height);
-    
-    
     // scale/align background - we want 100% width and vertical center
     imageView.contentMode = UIViewContentModeScaleAspectFit;
     CGRect frame = imageView.frame;
     frame.size.width = self.view.bounds.size.width;
-    
     CGFloat imageHeight = frame.size.height;
-    CGFloat imageWidth = frame.size.width;
-
-    
-    NSLog(@"image size height: %f", imageHeight);
-    NSLog(@"image size width: %f", imageWidth);
-    NSLog(@"screen height: %f", screenHeight);
-    NSLog(@"screen width: %f", screenWidth);
     
     // if the image is taller than the view, origin.y should be -1 * (half the image height - half the view height)
-    
     if (imageHeight > screenHeight) {
         CGFloat originY = -1 * (imageHeight/2 - screenHeight/2);
         NSLog(@"originY: %f", originY);
         frame.origin.y = originY;
     }
-    
-    // if the view is wider than the image, scale the image up
-    
-    if (screenWidth > imageWidth) {
-        float ratio = screenWidth / imageWidth;
-        frame.size.height *= ratio;
-        frame.size.width *= ratio;
-    }
-    
     imageView.frame = frame;
     
+    // split view into thirds with two red lines, first @ 1/3 view height, 2nd @ 2/3 view height
+    NSInteger lineMargin = 20;
+    UIColor *lineColor = [UIColor redColor];
+    
+    UIView *lineView1 = [[UIView alloc] initWithFrame:CGRectMake(lineMargin, (self.view.bounds.size.height * .33), self.view.bounds.size.width - (lineMargin * 2), 1)];
+    lineView1.backgroundColor = lineColor;
+    [self.view addSubview:lineView1];
+    
+    UIView *lineView2 = [[UIView alloc] initWithFrame:CGRectMake(lineMargin, (self.view.bounds.size.height * .67), self.view.bounds.size.width - (lineMargin * 2), 1)];
+    lineView2.backgroundColor = lineColor;
+    [self.view addSubview:lineView2];
+    
+    // footer text
+    UILabel *footerText = [[UILabel alloc] initWithFrame:CGRectMake(8.0f, (self.view.bounds.size.height * .67), 300.0f, 50.0f)];
+    footerText.textColor = [UIColor whiteColor];
+    [footerText setFont:[UIFont fontWithName:@"GillSans-Light" size:18]];
+    footerText.textAlignment = NSTextAlignmentCenter;
+    [footerText setText:[@"Shake or tap to generate" uppercaseString]];
+    [self.view addSubview:footerText];
     
 }
 
-- (void)handleGenerate: (UIButton *)buttonParam
+// we need two method signatures for handleGenerate because it is called in the two different contexts
+- (void)handleGenerate: (id *)param
 {
 
+    [self handleGenerate];
+}
+
+- (void)handleGenerate
+{
+    
     [self.labelBandName setText:[self getRandomBandName]];
 }
 
@@ -106,7 +113,7 @@
     NSString *adjective = [self getRandomStringFromArray:adjectives];
     NSString *noun = [self getRandomStringFromArray:nouns];
     
-    NSString *bandName = [NSString stringWithFormat:@"%@ %@", adjective, noun];
+    NSString *bandName = [NSString stringWithFormat:@"%@\n%@", adjective, noun];
     return bandName;
 }
 
@@ -120,6 +127,31 @@
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+// the following 4 methods support shaking gesture
+// as described: http://stackoverflow.com/a/2405692
+
+-(BOOL)canBecomeFirstResponder {
+    return YES;
+}
+
+-(void)viewDidAppear:(BOOL)animated {
+    [super viewDidAppear:animated];
+    [self becomeFirstResponder];
+}
+
+- (void)viewWillDisappear:(BOOL)animated {
+    [self resignFirstResponder];
+    [super viewWillDisappear:animated];
+}
+
+- (void)motionEnded:(UIEventSubtype)motion withEvent:(UIEvent *)event
+{
+    if (motion == UIEventSubtypeMotionShake)
+    {
+        [self handleGenerate];
+    }
 }
 
 @end
